2a34d13d73d1d104bd1b71136790c4a6f88c8ef8
Sveske_Juice <carl.benjamin.dreyer@gmail.com>
2023-12-07 20:00:27 +0100

    Hamming weight for n long bytes

--- include/hamming_functions.h
+++ include/hamming_functions.h
@@ -3,6 +3,10 @@
 
 #include <cstdint>
 
+int hamming_weight(uint8_t codeword);
+int hamming_weight(uint8_t* codeword, unsigned int size);
+
+int hamming_distance(uint8_t codeword1, uint8_t codeword2);
 int hamming_distance(uint8_t* codeword1, uint8_t* codeword2, unsigned int size);
 
 #endif
--- src/hamming_functions.cpp
+++ src/hamming_functions.cpp
@@ -1,14 +1,29 @@
 #include "hamming_functions.h"
+#include <cstdint>
+
+int hamming_weight(uint8_t *codeword, unsigned int size) {
+    unsigned int byteIdx = 0;
+    int dist = 0;
+
+    do {
+        for (uint8_t val = codeword[byteIdx]; val > 0; dist++) {
+            val &= (val - 1);
+        }
+    } while (++byteIdx < size);
+    return dist;
+}
+int hamming_weight(uint8_t codeword) { return hamming_weight(&codeword, 1); }
 
 int hamming_distance(uint8_t* codeword1, uint8_t* codeword2, unsigned int size) {
     unsigned int byteIdx = 0;
     int dist = 0;
 
     do {
-        for (unsigned int val = codeword1[byteIdx] ^ codeword2[byteIdx]; val > 0; dist++) {
+        for (uint8_t val = codeword1[byteIdx] ^ codeword2[byteIdx]; val > 0; dist++) {
             val &= (val - 1);
         }
     } while (++byteIdx < size);
 
     return dist;
 }
+int hamming_distance(uint8_t codeword1, uint8_t codeword2) { return hamming_distance(&codeword1, &codeword2, 1); }
