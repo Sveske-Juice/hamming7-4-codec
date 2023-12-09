babb2464c1b74efe5383baeaf3b986dcffe49ebd
Sveske_Juice <carl.benjamin.dreyer@gmail.com>
2023-12-09 17:04:23 +0100

    correct errors working

--- src/hamming7_4.cpp
+++ src/hamming7_4.cpp
@@ -1,27 +1,13 @@
 #include "hamming7_4.h"
 #include <bitset>
-#include <cstdio>
 #include <iostream>
-#include <ostream>
 #include <unordered_map>
 
 std::unordered_map<uint8_t, uint8_t> codewordToDecoded = {
-    {0b00000000, 0},
-    {0b11010010, 1},
-    {0b01010100, 2},
-    {0b10000110, 3},
-    {0b10011000, 4},
-    {0b01001010, 5},
-    {0b11001100, 6},
-    {0b00011110, 7},
-    {0b11100000, 8},
-    {0b00110010, 9},
-    {0b10110100, 10},
-    {0b01100110, 11},
-    {0b01111000, 12},
-    {0b10101010, 13},
-    {0b00101100, 14},
-    {0b11111110, 15},
+    {0b00000000, 0},  {0b11010010, 1},  {0b01010100, 2},  {0b10000110, 3},
+    {0b10011000, 4},  {0b01001010, 5},  {0b11001100, 6},  {0b00011110, 7},
+    {0b11100000, 8},  {0b00110010, 9},  {0b10110100, 10}, {0b01100110, 11},
+    {0b01111000, 12}, {0b10101010, 13}, {0b00101100, 14}, {0b11111110, 15},
 };
 
 // All the syndromes packed in a parity-check matrix
@@ -41,7 +27,7 @@ uint8_t encode_hamming7_4(uint8_t code) {
     return codeword;
 }
 
-int encode_hamming7_4(uint8_t* buffer, uint8_t* codeword, unsigned int size) {
+int encode_hamming7_4(uint8_t *buffer, uint8_t *codeword, unsigned int size) {
     for (int i = 0; i < size; i++) {
         uint8_t code = buffer[i];
 
@@ -58,16 +44,16 @@ int encode_hamming7_4(uint8_t* buffer, uint8_t* codeword, unsigned int size) {
         uint8_t c = code & (1 << 2);
         uint8_t d = code & (1 << 1);
 
-        uint8_t x = (a<<2)^(b<<4)^(d<<6);
-        uint8_t y = (a<<1)^(c<<4)^(d<<5);
-        uint8_t z = (b<<1)^(c<<2)^(d<<3);
+        uint8_t x = (a << 2) ^ (b << 4) ^ (d << 6);
+        uint8_t y = (a << 1) ^ (c << 4) ^ (d << 5);
+        uint8_t z = (b << 1) ^ (c << 2) ^ (d << 3);
 
         codeword[i] = code | x | y | z;
-        // std::cout << "Generated codeword from: " << std::bitset<8>(buffer[i]) << " = " << std::bitset<8>(codeword[i]) << std::endl;
+        // std::cout << "Generated codeword from: " << std::bitset<8>(buffer[i])
+        // << " = " << std::bitset<8>(codeword[i]) << std::endl;
     }
     return 0;
 }
-
 
 uint8_t decode_hamming7_4(uint8_t codeword) {
     uint8_t code = 0;
@@ -75,15 +61,61 @@ uint8_t decode_hamming7_4(uint8_t codeword) {
     return code;
 }
 
-int decode_hamming7_4(uint8_t* codewordBuf, uint8_t* outBuffer, unsigned int size) {
+int decode_hamming7_4(uint8_t *codewordBuf, uint8_t *outBuffer,
+                      unsigned int size) {
     for (int i = 0; i < size; i++) {
         uint8_t codeword = codewordBuf[i];
         // Check for valid codewords
-        if (auto it = codewordToDecoded.find(codeword); it != codewordToDecoded.end()) {
+        if (auto it = codewordToDecoded.find(codeword);
+            it != codewordToDecoded.end()) {
             outBuffer[i] = it->second;
-        }
-        else {
+        } else {
             // Codeword is not valid - try and fix it
+            uint8_t x = (codeword >> 7) & 1;
+            uint8_t y = (codeword >> 6) & 1;
+            uint8_t a = (codeword >> 5) & 1;
+            uint8_t z = (codeword >> 4) & 1;
+            uint8_t b = (codeword >> 3) & 1;
+            uint8_t c = (codeword >> 2) & 1;
+            uint8_t d = (codeword >> 1) & 1;
+
+            // Calculate the syndrome bits
+            uint8_t x1 = a ^ b ^ d;
+            uint8_t y1 = a ^ c ^ d;
+            uint8_t z1 = b ^ c ^ d;
+
+            std::cout << "\n\nCODEWORD: " << std::bitset<8>(codeword)
+                      << std::endl;
+            // Error in a
+            if (x != x1 && y != y1 && z == z1) {
+                codeword ^= 1 << 5;
+            }
+
+            // Error in b
+            else if (x != x1 && y == y1 && z != z1) {
+                codeword ^= 1 << 3;
+            }
+
+            // Error in c
+            else if (x == x1 && y != y1 && z != z1) {
+                codeword ^= 1 << 2;
+            }
+
+            // Error in d
+            else if (x != x1 && y != y1 && z != z1) {
+                codeword ^= 1 << 1;
+            }
+
+            std::cout << "CORRECTED codeword: " << std::bitset<8>(codeword)
+                      << std::endl;
+
+            // Write data bits to output positions 0b0000abcd
+            outBuffer[i] |= ((codeword >> 5) & 1) << 3; // a
+            outBuffer[i] |= ((codeword >> 3) & 1) << 2; // b
+            outBuffer[i] |= ((codeword >> 2) & 1) << 1; // c
+            outBuffer[i] |= ((codeword >> 1) & 1);      // d
+            std::cout << "CORRECTED: " << std::bitset<8>(outBuffer[i])
+                      << std::endl;
         }
     }
     return 0;
--- tests/hamming7_4.cpp
+++ tests/hamming7_4.cpp
@@ -3,7 +3,7 @@
 
 #include "hamming7_4.h"
 
-TEST_CASE( "Hamming (7,4) Encoding", "[hamming (7,4) encode]" ) {
+TEST_CASE("Hamming (7,4) Encoding", "[hamming (7,4) encode]") {
     // Encode tests
     REQUIRE(encode_hamming7_4(0b0101) == 0b01001010);
     REQUIRE(encode_hamming7_4(0b1111) == 0b11111110);
@@ -23,7 +23,7 @@ TEST_CASE( "Hamming (7,4) Encoding", "[hamming (7,4) encode]" ) {
     REQUIRE(memcmp(outBuffer2, correctCW2, 1024) == 0);
 }
 
-TEST_CASE( "Hamming (7,4) Decoding", "[hamming (7,4) decode]" ) {
+TEST_CASE("Hamming (7,4) Decoding", "[hamming (7,4) decode]") {
     // Decode tests
     REQUIRE(decode_hamming7_4(0) == 0);
     REQUIRE(decode_hamming7_4(0b01001010) == 0b101);
@@ -32,4 +32,16 @@ TEST_CASE( "Hamming (7,4) Decoding", "[hamming (7,4) decode]" ) {
     for (int i = 0; i < 15; i++) {
         REQUIRE(decode_hamming7_4(encode_hamming7_4(i)) == i);
     }
+
+    // abcd
+    // 1101
+    REQUIRE(decode_hamming7_4(0b01101010) == 0b0101); // Error in a
+    REQUIRE(decode_hamming7_4(0b10101110) == 0b1101); // Error in b
+
+    // abcd
+    // 0011
+    REQUIRE(decode_hamming7_4(0b11000110) == 0b0011); // Error in y
+    REQUIRE(decode_hamming7_4(0b10001110) == 0b0011); // Error in b
+    REQUIRE(decode_hamming7_4(0b10000010) == 0b0011); // Error in c
+    REQUIRE(decode_hamming7_4(0b10000100) == 0b0011); // Error in d
 }
