7a464c6ef0fc57f3beb3230160cbd87f1c61bf4c
Sveske_Juice <carl.benjamin.dreyer@gmail.com>
2023-12-15 14:06:40 +0100

    use 2 bytes to store encoded and decode to 1 byte

--- main.cpp
+++ main.cpp
@@ -1,15 +1,44 @@
 #include <bitset>
-#include <cstdint>
+#include <cstring>
 #include <iostream>
+#include <unistd.h>
 
 #include "hamming7_4.h"
 
 const char* msg = "Hello";
 
 int main(int argc, char** argv) {
-    for (int i = 0; i < 16; i++) {
-        uint8_t cw = encode_hamming7_4(i);
-        std::cout << std::bitset<8>(cw) << std::endl;
+    if (argc < 3) {
+        std::cerr << "Not enough arguments provided" << std::endl;
+        return 1;
+    }
+    else if (argc == 3 && strcmp(argv[1], "-e") == 0) {
+        size_t size = strlen(argv[2]);
+
+        uint8_t* codewordBuffer = (uint8_t*) malloc(sizeof(uint8_t)*size*2);
+        for (size_t i = 0; i < size; i++) {
+            encode_hamming7_4((uint8_t*) argv[2], codewordBuffer, sizeof(uint8_t)*size*2);
+        }
+
+        write(1, codewordBuffer, size*2);
+        for (int i = 0; i < size*2; i++) {
+            // std::cout << std::bitset<8>(*(codewordBuffer+i)) << " ";
+        }
+        // std::cout << std::endl;
+    }
+    else if (argc == 3 && strcmp(argv[1], "-d") == 0) {
+        size_t size = strlen(argv[2]);
+
+        uint8_t* decodedBuffer = (uint8_t*) malloc(sizeof(uint8_t)*size);
+        for (size_t i = 0; i < size; i++) {
+            decode_hamming7_4((uint8_t*) argv[2], decodedBuffer, sizeof(uint8_t)*size);
+        }
+
+        write(1, decodedBuffer, size);
+    }
+    else {
+        std::cerr << "Syntax error" << std::endl;
+        return 2;
     }
 
     return 0;
--- src/hamming7_4.cpp
+++ src/hamming7_4.cpp
@@ -21,17 +21,18 @@ uint8_t parityCheckMatrix[7][3] = {
     {1, 1, 1}, // Error in d
 };
 
-uint8_t encode_hamming7_4(uint8_t code) {
-    uint8_t codeword = 0;
-    encode_hamming7_4(&code, &codeword, 1);
-    return codeword;
-}
+// uint16_t encode_hamming7_4(uint8_t code) {
+//     uint16_t codeword = 0;
+//     // encode_hamming7_4(&code, &codeword, 1);
+//     return codeword;
+// }
 
 int encode_hamming7_4(uint8_t *buffer, uint8_t *codeword, unsigned int size) {
     for (int i = 0; i < size; i++) {
+        // FIRST 4 LSB
         uint8_t code = buffer[i];
 
-        // Go from 0 x y z a b c d -> x y z a b c d 0
+        // Go from b8 b7 b6 b5 b4 b3 b2 b1 -> b7 b6 b5 b4 b3 b2 b1 0
         code <<= 1;
 
         // Swap a and z in between y and z: x y z a b c d -> x y a z b c d p4
@@ -48,75 +49,102 @@ int encode_hamming7_4(uint8_t *buffer, uint8_t *codeword, unsigned int size) {
         uint8_t y = (a << 1) ^ (c << 4) ^ (d << 5);
         uint8_t z = (b << 1) ^ (c << 2) ^ (d << 3);
 
-        codeword[i] = code | x | y | z;
-        // std::cout << "Generated codeword from: " << std::bitset<8>(buffer[i])
-        // << " = " << std::bitset<8>(codeword[i]) << std::endl;
+        codeword[i*2] = code | x | y | z;
+
+        // LAST 4 LSB
+        code = buffer[i];
+
+        // Go from b8 b7 b6 b5 b4 b3 b2 b1 -> 0 0 0 b8 b7 b6 b5 b4
+        code >>= 3;
+        // rm b4
+        code &= ~0 ^ 1;
+
+        // Swap a and z in between y and z: x y z a b c d -> x y a z b c d p4
+        tmp = ((code >> 5) & 1) ^ ((code >> 4) & 1);
+        tmp = (tmp << 4) | (tmp << 5);
+        code ^= tmp;
+
+        a = code & (1 << 5);
+        b = code & (1 << 3);
+        c = code & (1 << 2);
+        d = code & (1 << 1);
+
+        x = (a << 2) ^ (b << 4) ^ (d << 6);
+        y = (a << 1) ^ (c << 4) ^ (d << 5);
+        z = (b << 1) ^ (c << 2) ^ (d << 3);
+        uint16_t lastpart = code | x | y | z;
+        codeword[i*2+1] = lastpart;
     }
     return 0;
 }
 
-uint8_t decode_hamming7_4(uint8_t codeword) {
-    uint8_t code = 0;
-    decode_hamming7_4(&codeword, &code, 1);
-    return code;
-}
+// uint8_t decode_hamming7_4(uint16_t codeword) {
+//     uint8_t code = 0;
+//     decode_hamming7_4(&codeword, &code, 1);
+//     return code;
+// }
 
 int decode_hamming7_4(uint8_t *codewordBuf, uint8_t *outBuffer,
                       unsigned int size) {
     for (int i = 0; i < size; i++) {
-        uint8_t codeword = codewordBuf[i];
-        // Check for valid codewords
-        if (auto it = codewordToDecoded.find(codeword);
-            it != codewordToDecoded.end()) {
-            outBuffer[i] = it->second;
-        } else {
-            // Codeword is not valid - try and fix it
-            uint8_t x = (codeword >> 7) & 1;
-            uint8_t y = (codeword >> 6) & 1;
-            uint8_t a = (codeword >> 5) & 1;
-            uint8_t z = (codeword >> 4) & 1;
-            uint8_t b = (codeword >> 3) & 1;
-            uint8_t c = (codeword >> 2) & 1;
-            uint8_t d = (codeword >> 1) & 1;
-
-            // Calculate the syndrome bits
-            uint8_t x1 = a ^ b ^ d;
-            uint8_t y1 = a ^ c ^ d;
-            uint8_t z1 = b ^ c ^ d;
-
-            std::cout << "\n\nCODEWORD: " << std::bitset<8>(codeword)
-                      << std::endl;
-            // Error in a
-            if (x != x1 && y != y1 && z == z1) {
-                codeword ^= 1 << 5;
-            }
-
-            // Error in b
-            else if (x != x1 && y == y1 && z != z1) {
-                codeword ^= 1 << 3;
-            }
-
-            // Error in c
-            else if (x == x1 && y != y1 && z != z1) {
-                codeword ^= 1 << 2;
-            }
-
-            // Error in d
-            else if (x != x1 && y != y1 && z != z1) {
-                codeword ^= 1 << 1;
-            }
-
-            std::cout << "CORRECTED codeword: " << std::bitset<8>(codeword)
-                      << std::endl;
-
-            // Write data bits to output positions 0b0000abcd
-            outBuffer[i] |= ((codeword >> 5) & 1) << 3; // a
-            outBuffer[i] |= ((codeword >> 3) & 1) << 2; // b
-            outBuffer[i] |= ((codeword >> 2) & 1) << 1; // c
-            outBuffer[i] |= ((codeword >> 1) & 1);      // d
-            std::cout << "CORRECTED: " << std::bitset<8>(outBuffer[i])
-                      << std::endl;
-        }
+        uint8_t lsb4 = decodeChunk(codewordBuf[i]);
+        uint8_t msb4 = decodeChunk(codewordBuf[++i]) << 4;
+        outBuffer[i] = lsb4 | msb4;
     }
     return 0;
+}
+
+uint8_t decodeChunk(uint8_t codeword) {
+    // Check for valid codewords
+    if (auto it = codewordToDecoded.find(codeword);
+        it != codewordToDecoded.end()) {
+        return it->second;
+    } else {
+        // Codeword is not valid - try and fix it
+        uint8_t x = (codeword >> 7) & 1;
+        uint8_t y = (codeword >> 6) & 1;
+        uint8_t a = (codeword >> 5) & 1;
+        uint8_t z = (codeword >> 4) & 1;
+        uint8_t b = (codeword >> 3) & 1;
+        uint8_t c = (codeword >> 2) & 1;
+        uint8_t d = (codeword >> 1) & 1;
+
+        // Calculate the syndrome bits
+        uint8_t x1 = a ^ b ^ d;
+        uint8_t y1 = a ^ c ^ d;
+        uint8_t z1 = b ^ c ^ d;
+
+        std::cout << "\n\nCODEWORD: " << std::bitset<8>(codeword) << std::endl;
+        // Error in a
+        if (x != x1 && y != y1 && z == z1) {
+            codeword ^= 1 << 5;
+        }
+
+        // Error in b
+        else if (x != x1 && y == y1 && z != z1) {
+            codeword ^= 1 << 3;
+        }
+
+        // Error in c
+        else if (x == x1 && y != y1 && z != z1) {
+            codeword ^= 1 << 2;
+        }
+
+        // Error in d
+        else if (x != x1 && y != y1 && z != z1) {
+            codeword ^= 1 << 1;
+        }
+
+        std::cout << "CORRECTED codeword: " << std::bitset<8>(codeword)
+                  << std::endl;
+
+        // Write data bits to output positions 0b0000abcd
+        uint8_t decoded = 0;
+        decoded|= ((codeword >> 5) & 1) << 3; // a
+        decoded|= ((codeword >> 3) & 1) << 2; // b
+        decoded|= ((codeword >> 2) & 1) << 1; // c
+        decoded|= ((codeword >> 1) & 1);      // d
+        std::cout << "CORRECTED: " << std::bitset<8>(decoded) << std::endl;
+        return decoded;
+    }
 }
